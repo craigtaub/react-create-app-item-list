c24845377584713eb481d3a125b06339266a63a1
Craig Taub <craigtaub@gmail.com>
2016-09-06 22:38:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const jokeList = ['first', 'second'];
+
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+        jokeList
+      }
+  }
+
+  getJoke() {
+    return `joke ${Math.random()}`;
+  }
+
+  onClickHandler = () => {
+      jokeList.push(this.getJoke());
+      this.setState({
+        jokeList
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <List list={jokeList}/>
+        <Button onClickHandler={this.onClickHandler} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+const List = ({list}) => (
+    <div>
+      {list.map((item) => {
+          return <Item item={item}/>;
+      })}
+
+    </div>
+);
+
+const Item = ({item}) => (
+    <div>
+        <span>{item}</span>
+    </div>
+);
+
+const Button = ({onClickHandler}) => (
+    <div>
+        <button onClick={onClickHandler}>Get Joke</button>
+    </div>
+);
